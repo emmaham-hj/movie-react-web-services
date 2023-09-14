d94272d34d39f63554dc0a9ecb4c497630b89e04
Emma's github <144634673+emmaham-hj@users.noreply.github.com>
2023-09-14 15:45:04 +1200

    Movie list population implementation.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+    console.log(json.data.movies);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          {movies.map((movie, index) => (
+            <div>
+              <h3>{movie.title}</h3>
+              <p>{movie.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
